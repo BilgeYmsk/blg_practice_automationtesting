7dec8ca457cd7635ae5a8a26638eeacd1311e016
Blgymsk <bilgeymsk@gmail.com>
2022-08-10 21:29:48 +0200

    Shop Page Update

--- src/test/java/com/practice_automationtesting/pages/ShopPage.java
+++ src/test/java/com/practice_automationtesting/pages/ShopPage.java
@@ -1,5 +1,37 @@
 package com.practice_automationtesting.pages;
 
+import com.practice_automationtesting.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
 public class ShopPage extends BasePage{
+
+    @FindBy(xpath = "//a[text()='Shop']")
+    public WebElement shopBtn;
+
+    @FindBy(xpath = "//span[@tabindex='0'][2]")
+    public WebElement priceLeft;
+
+    @FindBy(xpath = "//span[@tabindex='0'][1]")
+    public WebElement priceRight;
+
+
+    @FindBy(xpath = "//button[text()='Filter']")
+    public WebElement filterBtn;
+
+    @FindBy(xpath = "//div[@class='price_label']")
+    public WebElement priceText;
+
+//    public void price(double deger){
+//
+//        WebElement priceLeft= Driver.get().findElement(By.xpath("//span[@style='left: "+deger2+"%;']"));
+////        WebElement priceLeft= Driver.get().findElement(By.xpath("//span[@style='left: 0%;']"));
+////        Actions move = new Actions(Driver.get());
+////        Action action = (Action) move.dragAndDropBy(priceLeft, 150, 450).build();
+////        action.perform();
+//   }
 
 }
---
+++ src/test/java/com/practice_automationtesting/stepDefs/ShopStepDef.java
@@ -0,0 +1,59 @@
+package com.practice_automationtesting.stepDefs;
+
+import com.practice_automationtesting.pages.ShopPage;
+import com.practice_automationtesting.utilities.BrowserUtils;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+
+
+public class ShopStepDef {
+
+    ShopPage shopPage=new ShopPage();
+
+    @Then("the user click on Shop Menu")
+    public void theUserClickOnShopMenu() {
+        shopPage.navigateToMenu("Shop");
+        BrowserUtils.waitFor(1);
+    }
+
+    @And("Adjust the filter by price between number")
+    public void adjustTheFilterByPriceBetweenToRps() throws InterruptedException {
+
+//        Actions move = new Actions(Driver.get());
+//        Action action = move.dragAndDropBy(shopPage.priceRight, 60, 0).build();
+//        action.perform();
+
+        for(int i=150; i<200; i++){  //baslangic noktasi önemli sitede 150 den basliyor
+            shopPage.priceRight.sendKeys((Keys.ARROW_RIGHT));
+        }
+
+        for (int i = 0; i <50 ; i++) { //locater da yüzde deger görünüyor ama bu metodla adim adim ilerleme oldugundan istedigim rakam araligini yazabiliyorum
+            shopPage.priceLeft.sendKeys(Keys.ARROW_LEFT);
+        }
+
+        BrowserUtils.waitFor(2);
+
+    }
+
+    @And("Now click on Filter button")
+    public void nowClickOnFilterButton() throws InterruptedException {
+        Thread.sleep(2000);
+        shopPage.filterBtn.click();
+        BrowserUtils.waitFor(5);
+        
+    }
+
+    @Then("User can view books only between {int} to {int} rps price")
+    public void userCanViewBooksOnlyBetweenToRpsPrice(int min, int max) {
+
+        String actualText = shopPage.priceText.getText();
+        String expectedTex="Price: ₹"+min+" — ₹"+max;
+        Assert.assertEquals("The Message DO NOT match",expectedTex,actualText);
+
+
+    }
+
+
+}
