c8723e65d1920936ec19a0c2ba58945a10aeb3ff
Blgymsk <bilgeymsk@gmail.com>
2022-09-24 19:54:11 +0200

    CukesRunner Updated!

--- src/test/java/com/practice_automationtesting/runner/CukesRunner.java
+++ src/test/java/com/practice_automationtesting/runner/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue="com/practice_automationtesting/stepDefs",
         dryRun = false,
-        tags = "@Login"
+        tags = "  @order"
 )
 
 public class CukesRunner {
